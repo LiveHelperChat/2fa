1f5fd26aeb9da2a67557352c7cc1cc7aab90bd29
Remigijus Kiminas <remdex@gmail.com>
2023-09-07 06:35:03 -0400

    php 8.2

--- classes/erlhcoreclassmodel2fasession.php
+++ classes/erlhcoreclassmodel2fasession.php
@@ -1,5 +1,5 @@
 <?php
-
+#[\AllowDynamicProperties]
 class erLhcoreClassModel2FASession
 {
     use erLhcoreClassDBTrait;
--- classes/erlhcoreclassmodel2fauser.php
+++ classes/erlhcoreclassmodel2fauser.php
@@ -1,5 +1,5 @@
 <?php
-
+#[\AllowDynamicProperties]
 class erLhcoreClassModel2FAUser
 {
     use erLhcoreClassDBTrait;
